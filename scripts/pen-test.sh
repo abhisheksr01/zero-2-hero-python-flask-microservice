dccd5c74816e34e3921484acbdcfc502461375f9
abhisheksr01 <abhisheksingh2691@gmail.com>
2021-04-09 14:50:39 +0100

    update pen-test.sh

--- scripts/pen-test.sh
+++ scripts/pen-test.sh
@@ -10,4 +10,9 @@ echo "Running from containers home zap directory..."
 mkdir wrk
 
 echo "......................running zap......................"
-../tini -s -- zap-baseline.py -t "http://greetings.labs.armakuni.co.uk/health" -g gen.conf -r penetration-test-report.html || :
+
+zap-baseline.py -T 5 -w report.md -a -j -t "http://greetings.labs.armakuni.co.uk/health" | while read -r line; do if echo "${line}" | grep -q "FAIL-NEW: [0-9]+[\t ]+FAIL-INPROG: [0-9]+[\t ]+WARN-NEW: [0-9]+[\t ]+WARN-INPROG: [0-9]+[\t ]+INFO: [0-9]+[\t ]+IGNORE: [0-9]+[\t ]+PASS: [0-9]+" ; then pidof Xvfb | xargs -r kill ; fi ; echo "${line}" ; done
+EXIT_CODE="${?}"
+cat /zap/wrk/report.md
+pidof Xvfb | xargs -r kill
+exit "${EXIT_CODE}"
