0ed8e749c24b9d2e87e6f7a3a4afd117ba37eb47
abhisheksr01 <abhisheksingh2691@gmail.com>
2021-04-09 14:21:35 +0100

    update pen-test.sh

--- scripts/pen-test.sh
+++ scripts/pen-test.sh
@@ -7,4 +7,7 @@ pwd
 
 cd ../../../../zap/
 echo "Running from containers home zap directory..."
-mkdir wrk && zap-baseline.py -t "${ROOT_ENDPOINT}/health" -g gen.conf -r penetration-test-report.html || :
+mkdir wrk
+
+echo "......................running zap......................"
+../tini -s -- zap-baseline.py -t "http://greetings.labs.armakuni.co.uk/health" -g gen.conf -r penetration-test-report.html || :
