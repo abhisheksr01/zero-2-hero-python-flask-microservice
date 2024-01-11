e4e94822ed05d23ef6aef65f30f81e2145a6ab46
abhisheksr01 <abhisheksingh2691@gmail.com>
2024-01-11 15:04:30 +0000

    security(#103): fix cross site scripting vulnerability in code

--- greetings/greetings_generator.py
+++ greetings/greetings_generator.py
@@ -1,12 +1,14 @@
 import time
 
 from flask import Blueprint
+from markupsafe import escape
 
 greet = Blueprint("greet", __name__, url_prefix="/greetings/")
 
 
 @greet.route("/<user_name>", methods=["GET"])
 def get_greeting(user_name):
+    user_name = escape(user_name)
     current_hour = get_current_hour()
     day_state = "Night"
     if (current_hour >= 4) and (current_hour < 12):
